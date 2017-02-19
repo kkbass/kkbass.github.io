a3551ba202a178c68d6175d1f0e646e9abb8632e
kkbass <remboldtdesign@gmail.com>
2017-02-18 18:33:55 -0600

    Add files via upload

---
+++ js/playerTitles.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $('.audioplayer:nth-of-type(1)').append('<div class="right">\
+    <h3 class="performer">Wyanski</h3>\
+    <h3 class="title">Nothing that is not there and the nothing that is.</h3>\
+    <p class="song-info">Commissioned with Mathew Nichols, cello and Megan Lathan, viola</p>\
+  </div>');
+
+  $('.audioplayer:nth-of-type(2)').append('<div class="right">\
+    <h3 class="performer">Gubaidulina</h3>\
+    <h3 class="title">Pantomime.</h3>\
+    <p class="song-info">Krista Kopper, bass and Eun-Hee Park, piano</p>\
+  </div>');
+
+  $('.audioplayer:nth-of-type(3)').append('<div class="right">\
+    <h3 class="performer">Xenakis</h3>\
+    <h3 class="title">Roscobeck.</h3>\
+    <p class="song-info">Mathew Nichols, cello and Krista Kopper, bass</p>\
+  </div>');
+
+});
